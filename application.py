82c325113244d9cc7cbe679bfa9cf43da24c8744
Ashutoshpandey29 <ashutosh.pandey21b@iiitg.ac.in>
2024-08-02 16:12:37 +0530

    UI for fraud transactions

--- application.py
+++ application.py
@@ -171,14 +171,8 @@ def predict():
 
         # Convert results to HTML table and remove extra newlines
         results = df.to_html(classes='table table-striped', index=False).replace('\n', '')
-        
-        #Create a list of row classes based on prediction values
-        row_classes = ['bg-red' if val <= 0.9 else 'bg-green' for val in predictions]
-        
-        # Convert DataFrame to a list of lists for use in the template
-        table_data = df.values.tolist()
 
-        return render_template('results.html', table_data=table_data, row_classes=row_classes, columns=df.columns)
+        return render_template('results.html', tables=results)
 
 
 if __name__ == '__main__':
